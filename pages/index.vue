682457279a2ccf6e1c6d2dd648197efd5307ebb9
Smart Vain <zlc.blue15210@gmail.com>
2022-07-17 21:53:57 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-col>
+          <v-btn @click="getCaption()">get caption</v-btn>
+        </v-col>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      url: null,
+      html: null
+    }
+  },
+  methods: {
+    async getCaption() {
+      const html = await this.fetchUrlContent()
+      const regex = /"captionTracks":.*isTranslatable":(true|false)}]/;
+      const [match] = regex.exec(html);
+      
+      console.log([match])
+    },
+    async fetchUrlContent() {
+      return await this.$axios.$get('/api/watch', {params: {
+        v: 'ESUhsWGdpDQ'
+      }})
+    }
+  }
+}
+</script>
