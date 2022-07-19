794866d431060a7572fe63cf96a49195543cf8d2
Smart Vain <zlc.blue15210@gmail.com>
2022-07-20 08:31:07 +0900

    add: components

---
+++ components/LangSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-select
+    v-model="selectLang"
+    dense
+    item-text="text"
+    item-value="value"
+    :items="langList"
+    :loading="loading"
+    :no-data-text="noDataText"
+    :placeholder="placeholder"
+  >
+    <template #no-data>
+      <div v-if="loading" class="text-center">
+        <v-progress-circular color="primary" indeterminate/>
+      </div>
+
+      <div v-else class="text-center">
+        <span class="grey--text">{{ noDataText }}</span>
+      </div>
+    </template>
+  </v-select>
+</template>
+
+<script>
+export default {
+  props: {
+    langList: {
+      type: Array,
+      default: () => []
+    },
+    loading: {
+      type: Boolean,
+      default: () => false
+    }
+  },
+  data() {
+    return {
+      selectLang: '',
+      noDataText: 'URLが入力されていません。',
+      placeholder: '言語を選択'
+    }
+  }
+}
+</script>
---
+++ components/UrlInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <ValidationProvider
+    v-slot="{ errors }"
+    mode="passive"
+    name="URL"
+    rules="is_youtube_url"
+  >
+    <v-text-field
+      v-model="url"
+      dense
+      clearable
+      solo
+      append-icon="mdi-magnify"
+      :background-color="bgColor"
+      :error-messages="errors"
+      :flat="!isFocus"
+      :placeholder="placeholder"
+      @click:append="$emit('get-subtitle')"
+      @blur="isFocus = false; $emit('get-lang-list', url);"
+      @focus="isFocus = true"
+    />
+  </ValidationProvider>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isFocus: false,
+    placeholder: '字幕表示したいYouTube動画のURLを入力して下さい。',
+    url: ''
+  }),
+  computed: {
+    bgColor() {
+      const color = this.$vuetify.theme.dark ? 'blue-grey darken-2' : 'blue-grey lighten-5'
+      return this.isFocus ? color : null
+    }
+  },
+  created() {
+    this.url = 'https://www.youtube.com/watch?v=ouf7rXDlkDk'
+  }
+}
+</script>
