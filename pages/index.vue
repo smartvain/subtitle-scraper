42abd9c4dd8710a33b5e47aed2a480dade86653d
Smart Vain <zlc.blue15210@gmail.com>
2022-07-20 14:32:55 +0900

    tmp add

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,27 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card>
+        <div>url: {{ url }}</div>
+        <br>
+        <div>langList: {{ langList }}</div>
+        <br>
+        <div>selectedLang: {{ selectedLang }}</div>
+        <br>
+        <div>subtitle: {{ subtitle }}</div>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import SubtitleScraperMixin from '~/mixins/subtitle-scraper.js'
 
 export default {
   mixins: [ SubtitleScraperMixin ],
   name: 'IndexPage',
+  computed: {
+    ...mapGetters(['url', 'langList', 'selectedLang', 'subtitle'])
+  }
 }
 </script>
