999c54f065b170485f04bf4d60e17be29acd0e76
Smart Vain <zlc.blue15210@gmail.com>
2022-07-20 12:40:45 +0900

    refactor: chnage to vuex reference

--- components/UrlInput.vue
+++ components/UrlInput.vue
@@ -7,7 +7,7 @@
     style="width: 100%"
   >
     <v-text-field
-      v-model="url"
+      v-model="scopedUrl"
       dense
       clearable
       hide-details
@@ -16,27 +16,36 @@
       :error-messages="errors"
       :flat="!isFocus"
       :placeholder="placeholder"
-      @blur="isFocus = false; $emit('get-lang-list', url);"
+      @blur="isFocus = false; $emit('get-lang-list', scopedUrl);"
       @focus="isFocus = true"
     />
   </ValidationProvider>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  data: () => ({
-    isFocus: false,
-    placeholder: '字幕表示したいYouTube動画のURLを入力して下さい。',
-    url: ''
-  }),
+  data() {
+    return {
+      isFocus: false,
+      placeholder: '字幕表示したいYouTube動画のURLを入力して下さい。',
+    }
+  },
   computed: {
+    ...mapGetters(['url']),
+    scopedUrl: {
+      get() {
+        return this.url
+      },
+      set(string) {
+        this.$store.commit('setUrl', string)
+      }
+    },
     bgColor() {
       const color = this.$vuetify.theme.dark ? 'blue-grey darken-2' : 'blue-grey lighten-5'
       return this.isFocus ? color : null
     }
-  },
-  created() {
-    this.url = 'https://www.youtube.com/watch?v=ouf7rXDlkDk'
   }
 }
 </script>
