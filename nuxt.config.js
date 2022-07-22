f67b4d8497ec6a8fbdead5a693929c1243623cd9
Smart Vain <zlc.blue15210@gmail.com>
2022-07-22 20:50:01 +0900

    change: title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - y',
-    title: 'y',
+    titleTemplate: '%s',
+    title: 'Subtitle Scraper',
     htmlAttrs: {
       lang: 'en',
     },
