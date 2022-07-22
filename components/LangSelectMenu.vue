61471c1fe9034e7cc85ef417d8af16f296708ec9
Smart Vain <zlc.blue15210@gmail.com>
2022-07-22 15:08:56 +0900

    add: text of no lang-list

--- components/LangSelectMenu.vue
+++ components/LangSelectMenu.vue
@@ -4,21 +4,28 @@
     :rules="`required_selected:${selectedLang}`"
     style="width: 100%"
   >
-    <v-btn
-      icon
-      small
-      :loading="loading"
-      @click="isOpenMenu = !isOpenMenu"
-    >
-      <v-icon>mdi-translate</v-icon>
-    </v-btn>
-
     <v-menu
       v-model="isOpenMenu"
       offset-y
     >
+      <template #activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          icon
+          small
+          :loading="loading"
+          @click="isOpenMenu = !isOpenMenu"
+        >
+          <v-icon>mdi-translate</v-icon>
+        </v-btn>
+      </template>
+
       <v-list dense>
-        <v-list-item-group v-model="scopedSelectedLang">
+        <v-list-item-group
+          v-if="langList.length > 0"
+          v-model="scopedSelectedLang"
+        >
           <v-list-item
             v-for="(lang, idx) in langList"
             :key="idx"
@@ -29,6 +36,14 @@
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
+
+        <v-list-item v-else>
+          <v-list-item-content>
+            <v-list-item-title class="grey--text">
+              {{ $t('langSelectMenu.noDataText') }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-menu>
   </ValidationProvider>
