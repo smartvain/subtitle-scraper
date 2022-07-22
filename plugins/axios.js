51d235c6dff5b5aacfab79c74c816cc958239328
Smart Vain <zlc.blue15210@gmail.com>
2022-07-22 23:36:10 +0900

    change: axios error message

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,19 +1,11 @@
 export default ({ $axios, $toast }) => {
   $axios.onError(error => {
     const code = error.response.status
-    switch (code) {
-      case 401:
-        $toast.error(`${code}: ユーザーが承認されていません。先にログインをお試し下さい。`)
-        break
-      case 422:
-        $toast.error(`${code}: 不正な入力値を検知しました。`)
-        break
-      case 500:
-        $toast.error(`${code}: サーバーエラーが発生しました。時間をおいてもう一度お試しください。`)
-        break
-      default:
-        $toast.error('予期しないエラーが発生しました。時間をおいてもう一度お試しください。')
-        break
+
+    if (code >= 400 && code < 500) {
+      $toast.error(`${code}: An unexpected error has occurred. Please contact the developer.`)
+    } else {
+      $toast.error(`${code}: An unexpected error has occurred.`)
     }
   })
 }
