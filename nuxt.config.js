02663ae62d748cc2a7849e79f29501c4d16253f9
Smart Vain <zlc.blue15210@gmail.com>
2022-07-23 21:05:39 +0900

    install: vue-typed-js

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,8 +25,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/axios',
     '~/plugins/vee-validate',
-    '~/plugins/axios'
+    '~/plugins/vue-typed-js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,37 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="9" md="7">
-      <v-card>
+      <v-card v-if="subtitles.length > 0">
         <SubtitleDisplayer
           class="mt-3"
           @move-weblio="moveWeblio"
         />
       </v-card>
+
+      <vue-typed-js
+        :strings="['YouTube URL', 'Favorite video', 'Video to study']"
+        :loop="true"
+        :type-speed="140"
+        :back-speed="90"
+        class="main-text"
+      >
+        <p>Hello!<br>Please enter<br><span class="typing"></span></p>
+      </vue-typed-js>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import SubtitleDisplayer from '~/components/SubtitleDisplayer.vue'
 
 export default {
   name: 'IndexPage',
   components: {
-    SubtitleDisplayer
+    SubtitleDisplayer,
+  },
+  computed: {
+    ...mapGetters(['subtitles'])
   },
   methods: {
     moveWeblio(searchWord) {
