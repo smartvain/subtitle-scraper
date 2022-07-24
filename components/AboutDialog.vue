52c629d21c228c73fedf6029d3d28f1560bbf1d6
Smart Vain <zlc.blue15210@gmail.com>
2022-07-24 21:43:41 +0900

    create: about component

---
+++ components/AboutDialog.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-dialog v-model="scopedIsOpenDialog" width="670">
+    <v-card>
+      <v-card-title class="text-h6 font-weight-bold">
+        About this web site
+      </v-card-title>
+
+      <v-divider class="mb-5"/>
+
+      <v-card-text>
+        {{ $t('about.description1') }}<br>
+        {{ $t('about.description2') }}<br>
+        {{ $t('about.description3') }}
+      </v-card-text>
+
+      <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="scopedIsOpenDialog = !scopedIsOpenDialog"
+          >
+            close
+          </v-btn>
+        </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpenDialog: {
+      type: Boolean,
+      default: () => false
+    }
+  },
+  computed: {
+    scopedIsOpenDialog: {
+      get() {
+        return this.isOpenDialog
+      },
+      set(bool) {
+        this.$emit('update:isOpenDialog', bool)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
